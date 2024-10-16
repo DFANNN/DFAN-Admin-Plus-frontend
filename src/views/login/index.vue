11255f509afd8882d5f81ae7982ad289531b594c
DFANNN <737812379@qq.com>
2024-10-17 00:01:49 +0800

    feat(框架): 添加Element-plus
    1.添加Element-plus UI框架
    2.添加unplugin-vue-components 组件自动导入
    3.unplugin-auto-import 自动import
    4.忽略auto-imports.d.ts 和 components.d.ts

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>login组件</div>
+  <div>
+    login组件
+    <el-button type="primary" @click="showMessage">Primary</el-button>
+    <div>count:{{ count }}</div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElMessage } from 'element-plus'
+
+const count = ref(0)
+
+const showMessage = () => {
+  ElMessage.success('HELLO WORLD')
+  count.value++
+}
+</script>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server:{
-    open:'http://192.168.1.56:5173',
-    port:5173,
-    host:'0.0.0.0'
+  plugins: [
+    vue(),
+    AutoImport({
+      //自动import vue和vue-router
+      imports: ['vue', 'vue-router'],
+      // 声明文件生成位置和文件名称
+      dts: './auto-imports.d.ts',
+      // 这是为 Element Plus 组件库配置的解析器，自动按需导入 Element Plus 相关的 API
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  server: {
+    open: 'http://192.168.1.56:5173',
+    port: 5173,
+    host: '0.0.0.0'
   }
 })
