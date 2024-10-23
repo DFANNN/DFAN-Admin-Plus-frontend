d3251096b892e83ec8459991a280eb2b9b048e60
DFANNN <737812379@qq.com>
2024-10-23 22:09:33 +0800

    feat(框架): 添加路径别名

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,8 +22,8 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/login' },
-    { path: '/login', component: () => import('../views/login/index.vue') },
-    { path: '/home', component: () => import('../views/home/index.vue') }
+    { path: '/login', component: () => import('@/views/login/index.vue') },
+    { path: '/home', component: () => import('@/views/home/index.vue') }
   ]
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [
@@ -19,6 +20,16 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     })
   ],
+  resolve: {
+    // 路径别名
+    // import.meta.url 获取当前文件的路径，
+    // new URL(要解析的路径，当前文件路径) 为创建一个新的URL对象
+    // fileURLToPath(URL对象) 将URL对象转换为文件路径
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+
   server: {
     open: 'http://192.168.1.56:5173',
     port: 5173,
